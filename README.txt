e3684cb90dd3b1298816ea6ab669c7c0050ad565
Srinivasan V <60459000+Srini138@users.noreply.github.com>
2021-05-26 21:12:24 +0530

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,22 @@
+A disease is an unnatural medical condition that negatively affects the functional state of an organism and is generally associated with certain signs of illness. As reported by World Health Organization (WHO), Heart Disease and Stroke are the world’s biggest killers and have remained the leading causes of death globally in the last 15 years. In the direction of predicting heart disease, Machine Learning can present remarkable features that simplify the identification of unseen patterns, eventually providing clinical insights that assist physicians in planning and providing care.
+
+Data Description:
+This data set dates from 1988 and consists of four databases: Cleveland, Hungary, Switzerland, and Long Beach V. It contains 76 attributes, including the predicted attribute, but all published experiments refer to using a subset of 14 of them. The "target" field refers to the presence of heart disease in the patient. It is integer valued 0 = no disease and 1 = disease.
+
+
+Attribute Information:
+
+age
+sex
+chest pain type (4 values)
+resting blood pressure
+serum cholestoral in mg/dl
+fasting blood sugar > 120 mg/dl
+resting electrocardiographic results (values 0,1,2)
+maximum heart rate achieved
+exercise induced angina
+oldpeak = ST depression induced by exercise relative to rest
+the slope of the peak exercise ST segment
+number of major vessels (0-3) colored by flourosopy
+thal: 0 = normal; 1 = fixed defect; 2 = reversable defect
+The names and social security numbers of the patients were recently removed from the database, replaced with dummy values.
---
+++ app.py
@@ -0,0 +1,150 @@
+#Importing the necessary libraries
+from flask import Flask,render_template,request
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import pickle as pkl
+
+#Creating a flask name
+app=Flask(__name__,static_url_path='/static')
+
+#Loading the saved model 
+model=pkl.load(open('heart_prediction.pkl','rb'))
+
+#Routing the pages
+@app.route('/')
+def main():
+    return render_template('first.html')
+
+@app.route('/first')
+def first():
+    return render_template('first.html')
+
+@app.route('/prediction')
+def prediction():
+    return render_template('prediction.html')
+
+#Creating the object for StandardScaler
+sc=StandardScaler()
+
+
+@app.route('/result',methods=['POST'])
+def home():
+    #cp_1=0
+    cp_2=0
+    cp_3=0
+    restecg_1=0
+    age=int(request.form['age'])
+    sex_1=request.form['gender']
+    slope_1=0
+    ca_2=0
+    ca_3=0
+    ca_4=0
+    thal_2=0
+    thal_3=0
+    if(sex_1=='Female'):
+        sex_1=0
+    else:
+        sex_1=1
+    cp_1=request.form['cp']
+    if(cp_1=='Atypical Angina'):
+        cp_1=1
+        cp_2=0
+        cp_3=0
+    elif(cp_1=='Non Angina'):
+        cp_1=0
+        cp_2=1
+        cp_3=0
+    else:
+        cp_1=0
+        cp_2=0
+        cp_3=1
+    rating_bp=int(request.form['trestbps'])
+    chol=int(request.form['chol'])
+    fbs_1=request.form['fbs']
+    if(fbs_1=='Yes'):
+        fbs_1=1
+    else:
+        fbs_1=0
+    restecg_2=request.form['restecg']
+    if(restecg_2=='ST-T Wave Abnormality'):
+        restecg_1=1
+        restecg_2=0
+    else:
+        restecg_1=0
+        restecg_2=1
+    thalach=int(request.form['thalach'])
+    exang_1=request.form['exang']
+    if(exang_1=='Yes'):
+        exang_1=1
+    else:
+        exang_1=0
+    oldpeak=float(request.form['oldpeak'])
+    slope_2=request.form['slope']
+    if(slope_2=='Downsloping'):
+        slope_2=1
+        slope_1=0
+    else:
+        slope_1=1
+        slope_2=0
+    ca_1=request.form['ca']
+    if(ca_1==1):
+        ca_1=1
+        ca_2=0
+        ca_3=0
+        ca_4=0
+    elif(ca_1==2):
+        ca_1=0
+        ca_2=1
+        ca_3=0
+        ca_4=0
+    elif(ca_1==3):
+        ca_1=0
+        ca_2=0
+        ca_3=1
+        ca_4=0
+    else:
+        ca_1=0
+        ca_2=0
+        ca_3=0
+        ca_4=0
+    thal_1=request.form['thal']
+    if(thal_1=='Normal'):
+        thal_1=1
+        thal_2=0
+        thal_3=0
+    elif(thal_1=='Fixed Defect'):
+        thal_1=0
+        thal_2=1
+        thal_3=0
+    else:
+        thal_1=0
+        thal_2=0
+        thal_3=1
+        
+    #Scalarising the numerical data
+    numerical_data=(age,rating_bp,chol,thalach,oldpeak)
+    numerical_data_numpy=np.asarray(numerical_data)
+    numerical_data_reshape=numerical_data_numpy.reshape(1,-1)
+    std_data=sc.fit_transform(numerical_data_reshape)
+
+    #Reshapping the categorical data along with dummy variables
+    categorical_data=(sex_1,cp_1,cp_2,cp_3,fbs_1,restecg_1,restecg_2,exang_1,slope_1,slope_2,ca_1,ca_2,ca_3,ca_4,thal_1,thal_2,thal_3)
+    categorical_data_numpy=np.asarray(categorical_data)
+    categorical_data_reshape=categorical_data_numpy.reshape(1,-1)
+
+    #Predicting the new result
+    output=np.hstack([std_data,categorical_data_reshape])
+    predict=model.predict(output)
+    prediction=round(predict[0],2)
+
+    return render_template('result.html',data=prediction)
+    
+    
+
+if __name__=='__main__':
+    app.run(debug=True)
+
+
+
+    
